411e882b3ee0f2373fe894e19ad47e0ab7157872
Tuan Vuong <vanhtuan0409@gmail.com>
2019-12-03 09:49:06 +0700

    complete day1

--- 2019/day1/main.go
+++ 2019/day1/main.go
@@ -11,6 +11,14 @@ func calculateFuel(mass int) int {
 	return mass/3 - 2
 }
 
+func recursiveCalculateFuel(mass int) int {
+	requiredFuel := calculateFuel(mass)
+	if requiredFuel < 0 {
+		return 0
+	}
+	return requiredFuel + recursiveCalculateFuel(requiredFuel)
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	sum := 0
@@ -19,7 +27,7 @@ func main() {
 		if err != nil {
 			continue
 		}
-		sum += calculateFuel(mass)
+		sum += recursiveCalculateFuel(mass)
 	}
 	fmt.Println(sum)
 }
