a2c669310cd20a35ab885ba99b628a9f2cc3c240
Tuan Vuong <vanhtuan0409@gmail.com>
2020-12-10 14:20:49 +0700

    add day10 part2

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -1,6 +1,38 @@
 use std::collections::HashMap;
 use std::io::{self, BufRead};
 
+fn count_paths(
+    map: &mut HashMap<usize, u64>,
+    adapters: &[u32],
+    start_index: usize,
+    max_diff: u32,
+    target: usize,
+) -> u64 {
+    if start_index == target {
+        return 1;
+    }
+    if let Some(result) = map.get(&start_index) {
+        return result.clone();
+    }
+
+    let current_adapter = adapters[start_index];
+    let next_moves = adapters
+        .iter()
+        .skip(start_index + 1)
+        .enumerate()
+        .take_while(|(_, adapter)| *adapter - current_adapter <= max_diff)
+        .map(|(index, _)| start_index + index + 1)
+        .collect::<Vec<_>>();
+    let result = next_moves
+        .into_iter()
+        .rev()
+        .map(|index| count_paths(map, adapters, index, max_diff, target))
+        .sum();
+
+    map.insert(start_index, result);
+    result
+}
+
 fn main() {
     let mut adapters = io::stdin()
         .lock()
@@ -12,17 +44,20 @@ fn main() {
         .collect::<Vec<_>>();
     adapters.sort();
 
-    let mut diffs: HashMap<u32, u32> = HashMap::new();
-    let mut last = 0;
-    for adapter in adapters {
-        let diff = adapter - last;
-        *diffs.entry(diff).or_insert(0) += 1;
-        last = adapter
-    }
-    *diffs.entry(3).or_insert(0) += 1;
+    let max_diff = 3;
+    let mut map = HashMap::new();
+    let init_moves = adapters
+        .iter()
+        .enumerate()
+        .take_while(|(_, adapter)| **adapter <= max_diff)
+        .map(|(index, _)| index)
+        .collect::<Vec<_>>();
 
-    println!(
-        "{:?}",
-        diffs.get(&1).unwrap_or(&0) * diffs.get(&3).unwrap_or(&0)
-    );
+    let count: u64 = init_moves
+        .into_iter()
+        .rev()
+        .map(|index| count_paths(&mut map, &adapters, index, max_diff, adapters.len() - 1))
+        .sum();
+
+    println!("{}", count);
 }
